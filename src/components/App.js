03569d564a79b589a8bb4db8e48b9de071d66a71
indraneeldey2000 <116252120+indraneeldey2000@users.noreply.github.com>
2023-01-22 18:16:54 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/App.css';
 const App = () => {
-
+  const [count, setCount] = useState(0);
+  const increase = () => {
+    setCount(count => count + 1);
+  };
+  const decrease = () => {
+    if (count != 0) {
+      setCount(count => count - 1);
+    }
+  };
+  const reset = () => {
+    setCount(0)
+  }
   return (
     <div id="main">
       <h2>Task Counter</h2>
-      <button onClick={() => dispatch({type: 'add'})} id="addTaskBtn">Add a task</button>
-      <button onClick={() => dispatch({type: 'delete'})}  id="delTaskBtn">Delete a task</button>
-      <button onClick={() => dispatch({type: 'deleteAll'})}  id="delAllTaskBtn">Delete all tasks</button>
-      <h3 className="counter">Number of Tasks : {taskState.count}</h3>
+      <button onClick={increase} id="addTaskBtn">Add a task</button>
+      <button onClick={decrease} id="delTaskBtn">Delete a task</button>
+      <button onClick={reset} id="delAllTaskBtn">Delete all tasks</button>
+      <h3 className="counter">Number of Tasks : {count}</h3>
     </div>
   )
 }
